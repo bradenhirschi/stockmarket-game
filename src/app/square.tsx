464a9c4aa5d51c85778ec7e63dfd8f24730c6ab2
Braden Hirschi <bradenhirschi29@gmail.com>
2023-06-28 17:02:00 -0500

    Started creating square components

--- src/app/square.tsx
+++ src/app/square.tsx
@@ -1,7 +1,9 @@
-const Square = ({ company, colorClass, price }: { company: string, colorClass: string, price: number }) => {
-  return (<div className={`h-20 w-20 ${colorClass}`}>
-    {company}
-    ${price ? price.toLocaleString() :  '0'}
+const Square = (square: any) => {
+  return (<div className={`h-20 w-20 bg-${square.company ? square.company.color : 'white'}`}>
+    {square.type==='start' ? 'Start' : ''}
+    {square.type==='brokerFee' ? 'Start' : ''}
+    {square.type==='sellAll' ? 'Start' : ''}
+    {square.type==='stock' ? square.company.name : ''}
   </div>)
 }
 
